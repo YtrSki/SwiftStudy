aea78931a9546e569bb29d3ff273e60faa871e38
yut0124 <b1018133@fun.ac.jp>
2019-06-10 17:47:22 +0900

    0610

---
+++ NewsReader/NewsReader/ListViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ListViewController.swift
+//  NewsReader
+//
+//  Created by Yutaro Sakai on 2019/06/10.
+//  Copyright © 2019 Yutaro Sakai. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UITableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
